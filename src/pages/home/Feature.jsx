1e3522f54837b1057f5b7ece9ece1d5b49ed6f52
Josh Johnston <joshjohnston05@gmail.com>
2023-04-14 17:52:00 +1200

    added divs for feature section

---
+++ src/pages/home/Feature.jsx
@@ -0,0 +1,21 @@
+export default function Feature() {
+  return (
+    <>
+      <body>
+        <div className="feature-background">
+          <div className="feature-display" id="left-feature">
+            <h3 id="engagement-rings">ENGAGEMENT RINGS</h3>
+          </div>
+
+          <div className="feature-display" id="middle-feature">
+            <h3 id="signet-rings">SIGNET RINGS</h3>
+          </div>
+
+          <div className="feature-display" id="right-feature">
+            <h3 id="jewellery-sets">JEWELLERY SETS</h3>
+          </div>
+        </div>
+      </body>
+    </>
+  )
+}
